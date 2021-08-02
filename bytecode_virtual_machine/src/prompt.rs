026b0fdb4356146b731a3b832a15823fbd5f0c63
martin-fl <martinfleurial@gmail.com>
2021-08-02 13:05:12 +0200

    REPL now handles Left, Right and Delete keys

--- bytecode_virtual_machine/src/prompt.rs
+++ bytecode_virtual_machine/src/prompt.rs
@@ -4,8 +4,8 @@ use std::io;
 use crossterm::{
     self,
     cursor::{
-        self, MoveDown, MoveLeft, MoveRight, MoveTo, MoveToColumn, MoveToNextLine,
-        MoveToPreviousLine, MoveToRow, MoveUp,
+        self, CursorShape, MoveDown, MoveLeft, MoveRight, MoveTo, MoveToColumn, MoveToNextLine,
+        MoveToPreviousLine, MoveToRow, MoveUp, RestorePosition, SavePosition, SetCursorShape,
     },
     event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
     execute,
@@ -40,7 +40,11 @@ impl Prompt {
 
         terminal::enable_raw_mode().unwrap();
 
-        execute!(stdout, Print(&self.prompt))?;
+        execute!(
+            stdout,
+            Print(&self.prompt),
+            SetCursorShape(CursorShape::Line)
+        )?;
 
         let (left_, top_) = cursor::position()?;
         let (left, mut top) = (left_ as usize, top_ as usize);
@@ -64,9 +68,15 @@ impl Prompt {
             };
         }
 
+        let mut write_head = buffer.len();
+
         while let Event::Key(KeyEvent { code, modifiers }) = event::read()? {
             match (code, modifiers) {
-                (KeyCode::Char('c'), KeyModifiers::CONTROL) => return Ok(None),
+                (KeyCode::Char('c'), KeyModifiers::CONTROL) => {
+                    execute!(stdout, SetCursorShape(CursorShape::Block))?;
+                    terminal::disable_raw_mode().unwrap();
+                    return Ok(None);
+                }
                 (KeyCode::Enter, _) => {
                     if let KeyCode::Enter = prev {
                         execute!(stdout, MoveToColumn(0), Clear(ClearType::UntilNewLine))?;
@@ -91,48 +101,85 @@ impl Prompt {
                         }
                     }
                 }
-                //(KeyCode::Left, _) => {
-                //    if x!() > 0 {
-                //        execute!(stdout, MoveLeft(1))?;
-                //    }
-                //}
-                //(KeyCode::Right, _) => {
-                //    if x!() < line_end!() {
-                //        execute!(stdout, MoveRight(1))?;
-                //    }
-                //}
+                (KeyCode::Left, _) => {
+                    if x!() > 0 {
+                        execute!(stdout, MoveLeft(1))?;
+                        write_head -= 1;
+                    }
+                }
+                (KeyCode::Right, _) => {
+                    if x!() < line_end!() {
+                        execute!(stdout, MoveRight(1))?;
+                        write_head += 1;
+                    }
+                }
                 (KeyCode::Tab, _) => {
                     if x!() == line_end!() {
                         buffer.push('\t');
                         execute!(stdout, Print("\t"))?;
+                        write_head += 1;
                         line_end!() = x!();
                     }
                 }
                 (KeyCode::Backspace, _) => {
                     if buffer.len() > 0 {
-                        buffer.pop();
+                        if x!() == line_end!() {
+                            buffer.pop();
+                            write_head -= 1;
+                            line_end!() -= 1;
+                        } else if x!() > 0 && write_head > 0 {
+                            buffer.remove(write_head - 1);
+                            write_head -= 1;
+                            line_end!() -= 1;
+                        }
                     }
                     if x!() > 0 {
-                        execute!(stdout, MoveLeft(1), Print(" "), MoveLeft(1))?;
-                        line_end!() -= 1;
+                        execute!(
+                            stdout,
+                            MoveLeft(1),
+                            SavePosition,
+                            Clear(ClearType::FromCursorDown),
+                            Print(&buffer[write_head..]),
+                            RestorePosition
+                        )?;
                     }
                 }
-                //(KeyCode::Delete, _) => {
-                //    if 0 < x!() && x!() <= line_end!() {
-                //        execute!(stdout,Print(" "), MoveLeft(1))?;
-                //        line_end!() -= 1;
-                //        //TODO: shift the rest of the line to the left and remove char at right
-                //        //index
-                //    }
-                //}
+                (KeyCode::Delete, _) => {
+                    if buffer.len() > 0 {
+                        if x!() == line_end!() {
+                            buffer.pop();
+                            line_end!() -= 1;
+                        } else if x!() < line_end!() {
+                            buffer.remove(write_head);
+                            line_end!() -= 1;
+                        }
+                        execute!(
+                            stdout,
+                            SavePosition,
+                            Clear(ClearType::FromCursorDown),
+                            Print(&buffer[write_head..]),
+                            RestorePosition
+                        )?;
+                    }
+                }
                 (KeyCode::Char(c), _) => {
                     if x!() == line_end!() {
                         buffer.push(c);
                         line_end!() += 1;
+                        write_head += 1;
                         execute!(stdout, Print(c))?;
                     } else {
-                        //TODO: shift the rest of the line to the right and insert char into buffer
-                        //at right place
+                        buffer.insert(write_head, c);
+                        execute!(
+                            stdout,
+                            SavePosition,
+                            Clear(ClearType::FromCursorDown),
+                            Print(&buffer[write_head..]),
+                            RestorePosition,
+                            MoveRight(1)
+                        )?;
+                        line_end!() += 1;
+                        write_head += 1;
                     }
                 }
                 _ => {}
@@ -141,7 +188,7 @@ impl Prompt {
         }
 
         terminal::disable_raw_mode().unwrap();
-
+        println!("{}", &buffer);
         self.history.push(buffer.clone());
         Ok(Some(buffer))
     }
